191607cfcb1efa5ae183453e9d2ad996be191a85
himanshuchaurasiya24 <himanshuchaurasiya24@gmail.com>
2024-03-01 22:14:06 +0530

    mm

---
+++ dart_optionals/bin/dart_optionals.dart
@@ -0,0 +1,5 @@
+import 'package:dart_optionals/dart_optionals.dart' as dart_optionals;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_optionals.calculate()}!');
+}
---
+++ dart_optionals/bin/example1.dart
@@ -0,0 +1,26 @@
+//video stopped at 41:40
+void main(List<String> args) {
+  print('Hello');
+  // int age = 20;
+  // int? age1 = null;
+  // print(age);
+  // print(age1);
+
+  // // int age1 = null;
+  // void somthing(int age) {
+  //   print(age);
+  // }
+
+//
+  // somthing(int.parse(null));
+  // String? lastName;
+  // lastName ??= 'Bar';
+  // print(lastName);
+  // List<String?>? names = ['foo', null];
+  // print(names[1]?.length);
+  // print(names[1]);
+  final listpf = ['abc', 'abc', 'cde', 'def', 'cde', 'abc'];
+  print(
+    listpf.indexOf('cde'),
+  );
+}
